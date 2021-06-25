317f70552a663f51c7f5c4be6e42efd5f9dc97fd
Beric Bearnson <bericb@gmail.com>
2021-06-24 22:40:45 -0600

    ENH: Added responsive design and adjustable grid
    Created responsive square design and user adjustable resolution using a dropdown menu.

--- script.js
+++ script.js
@@ -1,31 +1,52 @@
 
 
 
-let x = 16
-let y = 16
+let x = 32;
+let y = 32;
 
-let variables = document.querySelector(":root");
-variables.style.setProperty('--main-cols', x)
-variables.style.setProperty('--main-cols', y)
+
 
 let grid = []
 
-const container = document.querySelector('.frame');
-console.log(container)
+let myList = [8,16,32,64,128]
 
-for (let i = 0; i < x; i++) {
-    for (let j = 0; j < y; j++) {
-        let newBoy = document.createElement('div')
-        newBoy.className = "cell";
-        newBoy.style.background = "silver";
-        newBoy.onmouseover = function() {newBoy.style.background = "grey"}
-        container.appendChild(newBoy)
-    }
+
+const container = document.querySelector('.frame');
+
+for (let i = 0; i < x * y; i++) {
+    let newBoy = document.createElement('div')
+    newBoy.className = "cell";
+    newBoy.style.background = "silver";
+    newBoy.onmouseover = function() {newBoy.style.background = "grey"}
+    container.appendChild(newBoy)
 }
 
 function clearGrid() {
     let children = container.children
     for (let i = 0; i < children.length; i++) {
         children[i].style.background = "silver";
+    }
+}
+
+function changeRes(value) {
+    console.log("SUPP")
+    x = value
+    y = value
+    document.documentElement.style.setProperty("--main-rows", x);
+    document.documentElement.style.setProperty("--main-cols", y);
+    console.log(x,y)
+    document.querySelectorAll('.cell').forEach(function(a){
+        a.remove()
+        })
+    initialize()
+}
+
+function initialize(){
+    for (let i = 0; i < x * y; i++) {
+        let newBoy = document.createElement('div')
+        newBoy.className = "cell";
+        newBoy.style.background = "silver";
+        newBoy.onmouseover = function() {newBoy.style.background = "grey"}
+        container.appendChild(newBoy)
     }
 }
